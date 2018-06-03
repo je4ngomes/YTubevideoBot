cdc2e09f38c79a2b7bb7f6a82cb17420dd87ac89
n1ghtw0lff <38874366+jeanstartx@users.noreply.github.com>
2018-06-02 21:22:05 -0300

    pipe function add

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,10 @@
 const randomId = () => '_' + Math.random().toString(36).substr(2, 9);
+const genDownloadLink = (format, id) => `https://youtubemp3api.com/@api/button/${format}/${id}`;
+const pipe = (funcs) => 
+    arg => funcs.reduce((prev, curr) => curr(prev), arg);
 
 module.exports = {
-    randomId
+    randomId,
+    pipe,
+    genDownloadLink
 };
