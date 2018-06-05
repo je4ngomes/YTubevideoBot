7420f7c61e6e4d4cae4e50a2790a30aa728c3171
Jean <38874366+n1ghtw0lff@users.noreply.github.com>
2018-06-05 01:01:21 -0300

    Update inlineHandler.js

--- bot/inlineHandler.js
+++ bot/inlineHandler.js
@@ -1,7 +1,7 @@
 const { searchVideo } = require('../models/youtube');
 const { formatResults } = require('../actions/inline');
 
-const inlineQuery = ({ inlineQuery, answerInlineQuery, message }) => {
+const inlineQuery = ({ inlineQuery, answerInlineQuery }) => {
     searchVideo(inlineQuery.query, 5)
         .then(formatResults)
         .then(answerInlineQuery)
