87b3a1960448f5ec01de4fcc669e09db95bc5175
Jean <38874366+nightw0lff@users.noreply.github.com>
2019-07-25 09:39:05 -0300

    Update youtube.js

--- models/youtube.js
+++ models/youtube.js
@@ -1,12 +1,11 @@
 const fetch = require('node-fetch');
 
-const searchVideo = (query='', limiter) => {
-    return fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${limiter}&q=${query}&key=${process.env.YT_KEY}`)
-            .then(res => res.json())
-            .catch(_ => {
-                throw 'API error while fetching the data.'
-            });
-};
+const searchVideo = (query='', limiter) =>
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${limiter}&q=${query}&key=${process.env.YT_KEY}`)
+        .then(res => res.json())
+        .catch(_ => {
+            throw 'API error while fetching the data.'
+        });
 
 module.exports = {
     searchVideo
